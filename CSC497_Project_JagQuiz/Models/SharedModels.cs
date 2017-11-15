e112933da4fcb54cb07181192ef2f859f6c32b5d
Schoolio <zps1101@jagmail.southalabama.edu>
2017-11-15 14:01:04 -0600

    fds

---
+++ CSC497_Project_JagQuiz/Models/SharedModels.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSC497_Project_JagQuiz.Models
+{
+    public struct student
+    {
+        string firstName;
+        string lastName;
+        string email;
+    }
+
+    public class AccountInformation
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string jagNumber;
+        public string accountType;
+
+        public AccountInformation(AppUser appUser)
+        {
+            this.firstName = appUser.FirstName;
+            this.lastName = appUser.LastName;
+            this.email = appUser.Email;
+            this.jagNumber = appUser.JagNumber;
+        }
+    }
+
+    public class PasswordConfirmation
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
